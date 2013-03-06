f269407a748b55ef739391d7c836676238acfac3
Michael Navat <navatm@gmail.com>
2013-03-06 17:23:22 +0200

    Examples added

---
+++ Server/upload_tests.py
@@ -0,0 +1,35 @@
+import base64
+from mechanize import Browser
+from bs4 import BeautifulSoup
+
+def saveResp(resp, stage):
+    lines = resp.readlines()
+    open(r"E:\Projects\Beny\temp%d.htm" % stage, "wb").write("".join(lines))
+
+url = r"http://physweb.bgu.ac.il/SUBMISSIONS/Scripts_bgu/upload.php?exnum=1&path=13A_Physics3_est_segel/"
+password = raw_input("password: ")
+filename = r"E:\Dropbox\Uni\Physics3\HomeWork1.pdf"
+
+br = Browser()
+
+# Authenticate upload page
+br.addheaders.append(('Authorization', 'Basic %s' % base64.encodestring('%s:%s' % ("navatm", password))))
+resp = br.open(url)
+saveResp(resp, 1)
+
+# Send file
+br.select_form(name="fileForm")
+br.form.add_file(open(filename,"rb"), 'text/plain', filename)
+resp = br.submit()
+saveResp(resp, 2)
+soup = BeautifulSoup(resp.get_data())
+
+# Fix HTML
+resp.set_data(soup.prettify(encoding='latin-1'))
+br.set_response(resp)
+saveResp(resp, 3)
+
+# Confirm file
+br.select_form(name="ConfirmForm")
+resp = br.submit()
+saveResp(resp, 4)
